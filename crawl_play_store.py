a300d4928b1aadd223e76b154e00a8d1a1256359
kangyunseo <kangyunseo@icloud.com>
2020-02-20 15:52:40 +0900

    change structure and add scroll function

--- crawl_play_store.py
+++ crawl_play_store.py
@@ -1,7 +1,4 @@
-
 # coding: utf-8
-
-# In[2]:
 
 import time
 from bs4 import BeautifulSoup
@@ -11,18 +8,57 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.proxy import *
 
 # @author Ranjeet Singh <ranjeetsingh867@gmail.com>
+# Modified my yunseo kang <kangyunseo@icloud.com>
 # Modify it according to your requirements
 
 no_of_reviews = 1000
 
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
-driver = webdriver.Chrome(r"C:\Users\user\Anaconda3\Scripts\chromedriver.exe")
+driver = webdriver.Chrome('/Users/kangyunseo/Downloads/chromedriver')
 
 wait = WebDriverWait( driver, 10 )
 
 
 # Append your app store urls here
 urls = ["https://play.google.com/store/apps/details?id=com.flipkart.android&hl=en"]
+
+"""
+ref from HUX at dev.to
+https://dev.to/hellomrspaceman/python-selenium-infinite-scrolling-3o12
+
+abstract
+This function do scrolling & click button to load all play store reviews
+
+requirement
+driver : webdriver object
+timeout : sleep time (sec) for loading one scrolled page
+max_scrolling : number of try scrolling
+"""
+def scroll(driver, timeout, max_scrolling):
+    scroll_pause_time = timeout
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    loopcounter = 0
+    while True:
+        loopcounter += 1
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        try:
+            driver.find_element_by_xpath( #click for next reviews
+                '//*[@id="fcxH9b"]/div[4]/c-wiz/div/div[2]/div/div[1]/div/div/div[1]/div[2]/div[2]/div/span/span').click()
+        except:
+            pass
+        # Wait to load page
+        time.sleep(scroll_pause_time)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height or loopcounter > max_scrolling:
+            # If heights are the same it will exit the function
+            # If scrolling number increase enough then exit the function
+            break
+        last_height = new_height
+
 
 for url in urls:
 
@@ -40,13 +76,14 @@ for url in urls:
 
     print("Installs Range: ", expatistan_table.string)
 
-    expatistan_table = soup_expatistan.find("meta", itemprop="ratingValue")
+    # Becausec play sotre web page get changed, need to change find option
 
-    print("Rating Value: ", expatistan_table['content'])
+    expatistan_table = soup_expatistan.find("div", class_="BHMmbe").get('aria-label')
+    print("Rating Value: ", expatistan_table)
 
-    expatistan_table = soup_expatistan.find("meta", itemprop="reviewCount")
-
-    print("Reviews Count: ", expatistan_table['content'])
+    expatistan_table = soup_expatistan.find("span", class_="EymY4b")
+    expatistan_table = expatistan_table.find_all("span")[1].get('aria-label')
+    print("Reviews Count: ", expatistan_table)
 
     soup_histogram = soup_expatistan.find("div", class_="VEF2C")
 
@@ -54,15 +91,14 @@ for url in urls:
 
     for rating_bar in rating_bars:
         print("Rating: ", rating_bar.find("span").text)
-        print("Rating count: ", rating_bar.find("span", class_="L2o20d").get('title'))
+        print("Rating count: ", rating_bar.find("span", class_="L2o20d").get('style'))
 
     # open all reviews
-    url = url+'&showAllReviews=true'
+    url = url + '&showAllReviews=true'
     driver.get(url)
-    time.sleep(5) # wait dom ready
-    for i in range(1,10):
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')#scroll to load other reviews
-        time.sleep(1)
+    time.sleep(5)  # wait dom ready
+    scroll(driver, 3, 50)
+    time.sleep(5)  # wait dom ready
     page = driver.page_source
 
     soup_expatistan = BeautifulSoup(page, "html.parser")
@@ -71,7 +107,7 @@ for url in urls:
     for expand_page in expand_pages:
         try:
             print("\n===========\n")
-            print("review："+str(counter))
+            print("review：" + str(counter))
             print("Author Name: ", str(expand_page.find("span", class_="X43Kjb").text))
             print("Review Date: ", expand_page.find("span", class_="p2TkOb").text)
             '''
@@ -89,10 +125,10 @@ for url in urls:
             print("Review Body: ", str(expand_page.find("div", class_="UD7Dzf").text))
             developer_reply = expand_page.find_parent().find("div", class_="LVQB0b")
             if hasattr(developer_reply, "text"):
-                print("Developer Reply: "+"\n", str(developer_reply.text))
+                print("Developer Reply: " + "\n", str(developer_reply.text))
             else:
                 print("Developer Reply: ", "")
-            counter+=1
+            counter += 1
         except:
             pass
 driver.quit()
